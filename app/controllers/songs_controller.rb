0b5940362e995022a86751967e9c1743043d706b
adkadziadka <industrialdog@gmail.com>
2017-05-30 01:05:42 +0200

    Add some songs (small step)

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -1,26 +1,54 @@
 class SongsController < ApplicationController
-  before_action :set_artist
+
   
   def new
     @song = Song.new
   end
   
-  def create
-    #artist_params = params.require(:artist).permit(:name, :age, :img_url)
+ def create
+    #song_params = params.require(:song).permit(:name, :age, :img_url)
 
-    @song = Song.new(params[:id])
+    @song = Song.new(song_params.merge(artist_id: params[:artist_id]))
 
     if @song.save
-      redirect_to @song
+      redirect_to artist_path(@artist.id), notice: "Song saved!"
     else
-      render 'new'
+      render :new
     end      
   end
-  
-  def set_artist
+    
+  def edit
+    @song = Song.find(params[:id])
   end
-  
-  
+
+  def update
+    @song = Song.find(params[:id])
+    #song_params = params.require(:song).permit(:name, :age, :img_url)
+
+    if @song.update_attributes(params[:id])
+      redirect_to @song, notice: "Song updated!"
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @song = Song.find(params[:id])
+
+    @song.destroy
+
+    redirect_to artist_song_path, notice: "Song deleted!"
+  end
+
+  private
+
+  def find_song
+    @song = Song.find(params[:id])
+  end
+
+
+    
 
 end
+
 

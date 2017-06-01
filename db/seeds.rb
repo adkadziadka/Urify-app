ba74348be6ce6b6fb5219c29f7adea3f8e0dd7ab
adkadziadka <industrialdog@gmail.com>
2017-06-01 07:51:42 +0200

    Adding seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,12 +1,15 @@
 Artist.destroy_all
 Song.destroy_all
 
-artist1 = Artist.create! ( { name: 'Shakira', 				age: 38, 		img_url: "https://goo.gl/5F51cg"} )
-artist2 = Artist.create! ( { name: 'Adele', 					age: 26, 		img_url: 'https://goo.gl/xtiHnO'} )
-artist3 = Artist.create! ( { name: 'Ed Sheran', 			age: 29, 		img_url: 'https://goo.gl/owwO5d'} )
-artist4 = Artist.create! ( { name: 'Bjork', 					age: 45, 		img_url: 'https://goo.gl/3e7lii'} )
-artist5 = Artist.create! ( { name: 'Paddy Kelly', 		age: 48, 		img_url: 'https://goo.gl/0CyLSn'} )
-artist6 = Artist.create! ( { name: 'Paloma Faith', 		age: 27, 		img_url: 'https://goo.gl/cyJmdk'} )
+artist1 = Artist.create! ( { name: 'Shakira', age: 38, img_url: "https://goo.gl/YqhkO6"} )
+artist2 = Artist.create! ( { name: 'Adele', age: 26, img_url: 'https://goo.gl/GFwyUG'} )
+artist3 = Artist.create! ( { name: 'Ed Sheeran',	age: 29, img_url: 'https://goo.gl/syUfsc'} )
+artist4 = Artist.create! ( { name: 'Jain', age: 23, img_url: 'https://goo.gl/iCxvGK'} )
+artist5 = Artist.create! ( { name: 'Paddy Kelly', age: 48, img_url: 'https://goo.gl/EQRCFA'} )
+artist6 = Artist.create! ( { name: 'Paloma Faith', age: 27, img_url: 'https://goo.gl/GhyXKB'} )
+artist7 = Artist.create! ( { name: 'Lady Gaga', age: 41, img_url: 'https://goo.gl/jdXdAC'} )
+artist8 = Artist.create! ( { name: 'Beirut', age: 30, img_url: 'https://goo.gl/RWEIyM'} )
+artist9 = Artist.create! ( { name: 'John Legend', age: 28, img_url: 'https://goo.gl/29Ljrd'} )
 
 
 
@@ -16,15 +19,6 @@ song3 = Song.create(title: "Gataway girl",				artist: artist3, 			description: "
 song4 = Song.create(title: "Cocoon",							artist: artist4, 			description: "Who would have known", 			year: "2001")
 song5 = Song.create(title: "Why dont you go",			artist: artist5, 			description: "If you have to live your life, I let you go", 			year: "1998")
 song6 = Song.create(title: "I cant rely on you", 	artist: artist6, 			description: "Some girl scream on reallu nice video", 			year: "2013")
-
-
-photo1 = Photo.create!( {img_url: "https://goo.gl/5F51cg", artist: artist1} )
-photo2 = Photo.create!( {img_url: "https://goo.gl/5F51cg", artist: artist1} )
-photo3 = Photo.create!( {img_url: "https://goo.gl/5F51cg", artist: artist1} )
-photo4 = Photo.create!( {img_url: "https://goo.gl/5F51cg", artist: artist1} )
-
-
-
 
 
 

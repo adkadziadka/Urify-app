9c56e598d6ff5d8c0fbdc9024cb973341e72822c
adkadziadka <industrialdog@gmail.com>
2017-06-02 13:43:42 +0200

    Fix artists migration to match schema

--- db/migrate/20170528130013_create_artists.rb
+++ db/migrate/20170528130013_create_artists.rb
@@ -1,7 +1,9 @@
 class CreateArtists < ActiveRecord::Migration[5.1]
   def change
     create_table :artists do |t|
-
+			t.string "name"
+			t.integer "age"
+			t.string "img_url"
       t.timestamps
     end
   end
